561271e4e8aa5a08aa5cabcde0f425b8dc2901be
Juan Dingevan <jidingevan@gmail.com>
2024-09-09 16:05:44 -0300

    Added constant to reflect the bugged fp arithmetic in my llvm build.

--- include/wingspan-constants.h
+++ include/wingspan-constants.h
@@ -1,5 +1,6 @@
 namespace ws {
 	namespace constants {
-		const bool AGGRESSIVE_OPTIMIZATIONS_ENABLED = true;
+		const bool AGGRESSIVE_OPTIMIZATIONS_ENABLED		= true;
+		const bool FLOATING_POINT_ARITHMETIC_IS_BUGGED	= true;
 	}
 }
