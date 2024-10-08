3a9ba14393e47075c23e7acb270bd2535a918956
Juan Dingevan <jidingevan@gmail.com>
2024-10-08 17:53:17 -0300

    Added ifdef guard to constants

--- include/wingspan-constants.h
+++ include/wingspan-constants.h
@@ -1,10 +1,16 @@
+#if !defined(WINGSPAN_CONSTANTS_H_)
+#define WINGSPAN_CONSTANTS_H_
+
 namespace ws {
 	namespace constants {
-		const bool	AGGRESSIVE_OPTIMIZATIONS_ENABLED		= true;
-		const bool	FLOATING_POINT_ARITHMETIC_IS_BUGGED		= true;
-		
-		const int	LOOP_INVARIANT_RECURSION_MAX_DEPTH		= 32;
+		const bool	AGGRESSIVE_OPTIMIZATIONS_ENABLED = true;
+		const bool	FLOATING_POINT_ARITHMETIC_IS_BUGGED = true;
 
-		const int	MAX_NUMBER_OF_LINES_FOR_INLINING		= 64;
+		const int	LOOP_INVARIANT_RECURSION_MAX_DEPTH = 32;
+		const int	MAX_NUMBER_OF_LINES_FOR_INLINING = 64;
+
+		const int	MAX_ITERATIONS_FOR_DEAD_CODE_DETECTION = 32;
 	}
 }
+
+#endif
